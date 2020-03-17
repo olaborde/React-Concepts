72a3f6c927a6e15c7a1eb4207c58a08e4cea9b3f
Osse Laborde <oml007@live.com>
2020-03-16 22:11:46 -0400

    added bootstrap

--- reduxapp/src/components/displayComponent.js
+++ reduxapp/src/components/displayComponent.js
@@ -9,8 +9,8 @@ const Display = (props) => {
             return datalist.map((item) => {
                 return(
                  
-                    <div className="card" style={{width: '18rem'}}  key={item.id}>
-                    <img src={item.img} className="card-img-top img-fluid" alt="Responsive" />
+                    <div className="card" style={{width: '15rem'}}  key={item.id}>
+                    <img src={item.img} className="card-img-top img-fluid" height={'200px'} alt="Responsive" />
                     <div className="card-body">
                         <h5 className="card-title">{item.name}</h5>
                         
